081c3317c552c3b0d0170576eec6fd8824e8d54b
Aayushi <aayushi265@gmail.com>
2022-12-28 23:36:13 +0530

    training

---
+++ Weekend Task 1 - With Navigation(V6)/Selectorswitch.js
@@ -0,0 +1,48 @@
+import { View, TouchableOpacity } from 'react-native'
+import React, {Component} from 'react'
+import SwitchSelector from "react-native-switch-selector";
+import Style from './Style';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+const SelectorSwitch = [
+    { label: "LIST", value: "list" },
+    { label: "MAP", value: "map" },
+  ];
+  
+  class Selectorswitch extends Component {
+    render() {
+      return(
+        <View>
+        <View style= {Style.container}>
+          <SwitchSelector  
+          options={SelectorSwitch}
+          initial={0}
+          onPress={value => console.log(`Call onPress with value: ${value}`)}
+          textColor = "#5272B4"
+          selectedColor = "white"
+          buttonColor = "#1B5AA5"
+          borderColor = "#3F72B0"
+          fontSize = {13}
+          borderRadius = {8}
+          height = {30}
+          valuePadding = {1.5}
+          
+          
+          hasPadding
+          />
+          <View style={Style.iconstyle}>
+            <TouchableOpacity onPress={()=>this.props.navigation.navigate("ButtonClicked")}>
+          <MaterialCommunityIcons name="magnify" size={30} color="#1757A2" />
+          </TouchableOpacity>
+          </View>
+          
+        </View>
+        <View style={Style.hairline} />
+          
+          
+        </View>
+      )
+    }
+    
+  }
+
+  export default Selectorswitch;
